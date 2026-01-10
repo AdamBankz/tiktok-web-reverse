682b933668d205d77a80318a8917f840d9d1f2ae
adam <anniebwby@gmail.com>
2026-01-10 11:41:06 +0000

    Update verifyFp.py

--- Security Headers (Signatures)/verifyFp.py
+++ Security Headers (Signatures)/verifyFp.py
@@ -1,8 +1,7 @@
-def get_fp():
-    """ tiktok - web Client devices """
-    import time, random, string
+import time, random, string
+
+def verifyFp():
     def to_base36(value):
-        """Convert an integer to a hexadecimal string"""
         if value == 0:
             return "0"
 
@@ -15,14 +14,10 @@ def get_fp():
 
         return ''.join(reversed(base36))
 
-    # Defining a character set
     e = list(string.digits + string.ascii_uppercase + string.ascii_lowercase)
     t = len(e)
-    # Get the current timestamp and convert it to 36 hexadecimal string
     r = to_base36(int(time.time() * 1000))
-    # Initialize the result list
     n = [None] * 36
-    # Set the character at a specific position
     n[8] = n[13] = n[18] = n[23] = "_"
     n[14] = "4"
     for i in range(36):
@@ -32,11 +27,11 @@ def get_fp():
                 n[i] = e[(o & 3) | 8]
             else:
                 n[i] = e[o]
-    # Splicing the final result
     result = "verify_" + r + "_" + ''.join(n)
     return result
 
 
 
 if __name__ == '__main__':
-    print(get_fp())
+
+    print(verifyFp())
